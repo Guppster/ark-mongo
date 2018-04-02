a9be7541d0c16244e16d421d645ac0573e5cea99
Gurpreet Singh <gurpreet@mailfence.com>
2018-04-01 19:01:54 -0500

    Made hash output super nice

--- arkmongo.gemspec
+++ arkmongo.gemspec
@@ -49,6 +49,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency "pastel", "~> 0.7.2"
   spec.add_dependency "thor", "~> 0.19.4"
   spec.add_dependency "mongo", "~> 2.4"
+  spec.add_dependency "neatjson"
+
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/arkmongo/commands/hash.rb
+++ lib/arkmongo/commands/hash.rb
@@ -6,6 +6,7 @@ require 'mongo'
 require 'digest'
 require 'json'
 require 'ark'
+require 'neatjson'
 
 module Arkmongo
   module Commands
@@ -19,6 +20,8 @@ module Arkmongo
 
         pastel = Pastel.new
         @output = pastel.blue.detach
+        @db_output = pastel.cyan.detach
+        @blockchain_output = pastel.green.detach
         @error = pastel.red.detach
         @timestamp = pastel.yellow.detach
 
@@ -36,14 +39,24 @@ module Arkmongo
       end
 
       def display(output, level)
-        puts @timestamp.call(Time.now.strftime("%H:%M:%S")) + ': ' + level.call(output)
+
+        if level == @blockchain_output
+          label = '[ARK]: '
+        elsif level == @db_output
+          label = '[DB]: '
+        elsif level == @output
+          label = '[ArkMongo]: '
+        elsif level == @error
+          label = '[WARNING]: '
+        end
+
+        puts @timestamp.call(Time.now.strftime("%H:%M:%S")) + ': ' + level.call(label + output)
       end
 
       def execute
         puts '------------------------------------------------------------------'
         puts
         display('Executing Hash command', @output)
-        display('BIG ERROR OMG', @error)
 
         # Create a new DB for storing hashes (hashDB)
         init_hash_db
@@ -61,12 +74,16 @@ module Arkmongo
       # Creates a database to store previous queries and their hashes for
       # validation in the future
       def init_hash_db
+
+        display('Initializing HashDB', @output)
         # Get a new client that handles the 'arkmongo' database
         @hash_client = @client.use(:arkmongo)
         @hash_collection = @hash_client.database[:query_hashes]
 
         # Use an index view to setup indexes for the new collection
         hash_index_view = Mongo::Index::View.new(@hash_collection)
+
+        display('Creating query_hashes collection indexes', @db_output)
 
         # TODO: change to create_many method
         hash_index_view.create_one({ db: 1, collection: 1, query: 1,
@@ -80,6 +97,7 @@ module Arkmongo
 
       # Returns the hash of the database query specified on initialization
       def generate_hash
+        display('Generating document hash', @output)
         # Setup hashing function
         sha256 = Digest::SHA256.new
 
@@ -93,6 +111,8 @@ module Arkmongo
         collection.find(@options[:query]).each do |document|
           sha256 << document.to_s
         end
+        
+        display("Query hash is #{sha256.hexdigest}", @output)
 
         # Return the hash
         sha256.hexdigest
@@ -100,6 +120,7 @@ module Arkmongo
 
       # Saves the hash
       def save_hash(hash)
+        display('Saving document hash', @output)
         # Save the hash to the hash database
         save_hash_db(hash)
 
@@ -117,6 +138,7 @@ module Arkmongo
 
         # Update the query with hash_data
         update_db(hash_data)
+        display('Successfully saved to caching DB', @db_output)
       end
 
       def update_db(update_hash,
@@ -131,12 +153,15 @@ module Arkmongo
           projection: projection
         }
 
+        display("updating cache db with \n" + JSON.neat_generate(update_hash) + "\nfor query\n" + JSON.neat_generate(filter_data), @db_output)
+
         # Update the query with new hash data
         @hash_collection.update_one(filter_data, { '$set' => update_hash }, upsert: true)
       end
 
       def save_hash_ark(hash)
         process_transaction(hash)
+        display('Successfully sent to blockchain', @output)
       end
 
       def process_transaction(hash)
@@ -144,17 +169,29 @@ module Arkmongo
         public_key = retrieve_public_key.to_s
         secret = generate_secret
 
+        display("Query address is #{public_key}", @blockchain_output)
+
         update_db(address: public_key,
                   secret: secret,
                   blockStatus: 'signed and awaiting confirmation')
 
+        display('Updated cache DB with blockchain details', @output)
+
+        display('Creating ark transaction', @blockchain_output)
         # Create and send out a transaction
         @ark.create_transaction(public_key, 1, hash, @delegate_secret, nil)
       end
 
       # Return the public key corrisponding to this query's key
       def retrieve_public_key
-        full_key = Ark::Util::Crypto.get_key(generate_secret)
+        display('Generating public key', @blockchain_output)
+
+        secret = generate_secret
+        display("Query secret is #{secret}", @blockchain_output)
+
+        full_key = Ark::Util::Crypto.get_key(secret)
+
+        display('Prefix 1E (30)/(D) is being used which is already associated with dArk', @error)
         Ark::Util::Crypto.get_address(full_key, '1E')
       end
 
@@ -162,6 +199,7 @@ module Arkmongo
       # This isn't bad because the value on the address is useless to us
       # We only care about transaction integrity which happens regardless of individual secrets
       def generate_secret
+        display('Generating secret', @blockchain_output)
         Digest::SHA256.hexdigest "xx#{@client.database.name}xx#{@collection_name}xx#{@options[:query]}xx"
       end
 
